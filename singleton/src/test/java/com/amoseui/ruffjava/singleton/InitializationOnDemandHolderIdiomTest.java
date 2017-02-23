1182836b5f01881958f5cb38614a441ee75e5f88
amoseui <amoseui@gmail.com>
2017-02-23 18:30:00 +0900

    Rename package pragmaticjava to ruffjavaruffjavaruffjavaruffjava

--- singleton/src/test/java/com/amoseui/ruffjava/singleton/InitializationOnDemandHolderIdiomTest.java
+++ singleton/src/test/java/com/amoseui/ruffjava/singleton/InitializationOnDemandHolderIdiomTest.java
@@ -1,4 +1,4 @@
-package com.amoseui.pragmaticjava.singleton;
+package com.amoseui.ruffjava.singleton;
 
 public class InitializationOnDemandHolderIdiomTest extends SingletonTestBase<InitializationOnDemandHolderIdiom> {
 
